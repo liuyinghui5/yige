7b7819f9ef9f824112edac41d98c039e334e105a
CaoYi <caoyi5225@sina.com>
2018-10-26 14:28:26 +0800

    修复锚点无效bug

--- static/js/scrollIntoView.js
+++ static/js/scrollIntoView.js
@@ -1,1 +1,18 @@
-/** * Created by liuyinghui on 18/10/26. */// 封装的scrollIntoView 方法抛出供VUE项目使用export default function scrollF(domD,move) {	let getDom = document.querySelector(`${domD}`);	let getMove = document.querySelector(`${move}`);	console.log('获取的domD,move ->', getDom,getMove)	getMove.addEventListener('click', function () {		console.log('打印的添加监听')		getDom.scrollIntoView({			block: 'start',			behavior: 'smooth'		});	});}
+/**
+ * Created by liuyinghui on 18/10/26.
+ */
+// 封装的scrollIntoView 方法抛出供VUE项目使用
+
+export default function scrollF(domD,move) {
+	let getDom = document.querySelector(`${domD}`);
+	let getMove = document.querySelector(`${move}`);
+	console.log('获取的domD,move ->', getDom,getMove)
+	getDom.addEventListener('click', function () {
+		console.log('打印的添加监听')
+		getMove.scrollIntoView({
+			block: 'start',
+			behavior: 'smooth'
+		});
+	});
+}
+
