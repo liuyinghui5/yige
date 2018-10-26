54d1e341c0fed9ad1c8116f3bee59aac1b1d22a9
英辉 刘 <huidi88888888@163.com>
2018-10-26 10:57:01 +0800

    UPDATE

--- src/router/index.js
+++ src/router/index.js
@@ -6,38 +6,40 @@ import two from '@/pages/two'
 import three from '@/pages/three'
 import four from '@/pages/four'
 import lodash from '@/pages/lodash'
-
-
-
+import scroll from '@/pages/scrollIntoViewTest'
 
 Vue.use(Router)
 
 export default new Router({
-    routes: [
-        {
-            path: '/',
-            name: 'HelloWorld',
-            component: HelloWorld
-        }, {
-            path: '/one',
-            name: 'one',
-            component: one
-        }, {
-            path: '/two',
-            name: 'two',
-            component: two
-        }, {
-            path: '/three',
-            name: 'three',
-            component: three
-        }, {
-            path: '/four',
-            name: 'four',
-            component: four
-        }, {
-            path: '/lodash',
-            name: 'lodash',
-            component: lodash
-        }
-    ]
+	routes: [
+		{
+			path: '/',
+			name: 'HelloWorld',
+			component: HelloWorld
+		}, {
+			path: '/one',
+			name: 'one',
+			component: one
+		}, {
+			path: '/two',
+			name: 'two',
+			component: two
+		}, {
+			path: '/three',
+			name: 'three',
+			component: three
+		}, {
+			path: '/four',
+			name: 'four',
+			component: four
+		}, {
+			path: '/lodash',
+			name: 'lodash',
+			component: lodash
+		}, {
+			path: '/scroll',
+			name: 'scroll',
+			component: scroll
+		}
+	]
 })
